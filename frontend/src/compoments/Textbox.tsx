cde88c29fc38acf6408ad21e9a46e8f14e35723c
NewToProgramming58 <107695464+NewToProgramming58@users.noreply.github.com>
2023-05-28 10:27:01 +0200

    Burger menu rotate and finisihing touches n textbox

--- frontend/src/compoments/Textbox.tsx
+++ frontend/src/compoments/Textbox.tsx
@@ -1,21 +1,24 @@
 import React, { ChangeEvent, useState } from 'react'
 
-export default function Textbox() {
+interface TextboxProps {
+  text: string;
+}
+export default function Textbox(TextboxProps: TextboxProps) {
+  const text: string = '';
   const [input, setInput] = useState<string>('');
   const [focused, setFocused] = useState<boolean>(false);
   return (
     <div className='relative'>
-      <label htmlFor='myInput' className={`text-white absolute left-[5%] ${focused ? 'top-[-10%]' : 'top-0'}transition-all duration-200 ease-in-out m-[1%]`}>
-        Floating Label
-      </label>
       <input type="text" id="myInput"
-        onFocus={() => {
-          setFocused((prev) => { return !prev })
-        }}
-        className="bg-gray-100 bg-opacity-30 border-b border-gray-300 focus:border-gray-400 outline-none p-[1%] rounded-md hover:bg-opacity-50 transition-colors duration-300 ease-out"
+        onFocus={() => {setFocused(true);}}
+        onBlur={() => { setFocused(false); }}
+        className={`bg-gray-100 ${focused ? 'bg-opacity-70' : 'bg-opacity-30'} border-b border-gray-300 focus:border-gray-400 outline-none p-[5%] rounded-md hover:bg-opacity-70 transition-colors duration-300 ease-out`}
         value={input}
         onChange={(e) => setInput(e.target.value)}
       />
+      <label htmlFor='myInput' className={`text-white transition-all duration-300 ease-in-out absolute left-[5%] ${focused ? '-top-[75%]' : 'top-[15%]'} m-[1%]`}>
+        {TextboxProps.text}
+      </label>
     </div>
   )
 }
