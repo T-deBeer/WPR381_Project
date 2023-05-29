5d8a9cba6b91f2c1f028a5dc6a8b4ac8296cd294
T-deBeer <tdebeer.za@gmail.com>
2023-05-29 15:45:53 +0200

    Fixed bugs

--- frontend/src/compoments/Textbox.tsx
+++ frontend/src/compoments/Textbox.tsx
@@ -1,5 +1,5 @@
 import React, { ChangeEvent, useState } from 'react'
-import TextboxProps from '../data/interfaces';
+import { TextboxProps }from '../data/interfaces';
 
 export default function Textbox(TextboxProps: TextboxProps) {
   const text: string = '';
--- frontend/src/data/interfaces.tsx
+++ frontend/src/data/interfaces.tsx
@@ -1,3 +1,13 @@
-export default interface TextboxProps{
+export interface TextboxProps{
     text: string;
   }
+  
+export interface User {
+    firstName: string;
+    lastName: string;
+  }
+  
+export interface BackendData {
+    users: User[];
+  }
+  
