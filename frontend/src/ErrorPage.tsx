17615c5294b988c00f7837c3257ed1017454b311
Jacques <jacquesvdw2002@gmail.com>
2023-06-04 17:16:42 +0200

    a

--- frontend/src/ErrorPage.tsx
+++ frontend/src/ErrorPage.tsx
@@ -10,6 +10,7 @@ export default function ErrorPage() {
           <h1 className=' text-[6rem]'>404</h1>
           <h2 className='text-[3rem]'>It seems you are trying to predict the weather of a place that is not on planet earth</h2>
           <h2 className='text-[2rem]'>Please use the navigation menu on the top left to navigate back to the home page</h2>
+          <Button icon="Go back to Home" onClick={() => {}} hint='Go back to home' className=''/> 
         </div>
       </div>
   
