8518ec4cda0555eeb78bd15895e065131342c599
T-deBeer <tdebeer.za@gmail.com>
2023-06-05 10:35:47 +0200

    One more change

--- frontend/src/assets/index.ts
+++ frontend/src/assets/index.ts
@@ -6,23 +6,23 @@ import cloudy from '/Cloudy.png';
 import rain from '/Rain.png';
 import snow from '/Snow.png';
 import sunny from '/Sunny.png'
-import tempNormal from './tempNormal.png';
-import tempWarm from './tempWarm.png';
-import tempCold from './tempCold.png';
-import thunderstorm from './Thunderstorm.png';
-import wind from './wind.png';
-import fog from './fog.png';
-import sunrise from './sunrise.png';
-import sunset from './sunset.png';
-import moonrise from './moonrise.png';
-import moonset from './moonset.png';
-import compass from './compass.png';
-import gust from './gust.png';
-import humidity from './humidity.png';
-import feel from './feel.png';
-import sunny_video from './sunny.mp4';
-import rainy_video from './rainy.mp4';
-import cloudy_video from './cloudy.mp4';
+import tempNormal from '/tempNormal.png';
+import tempWarm from '/tempWarm.png';
+import tempCold from '/tempCold.png';
+import thunderstorm from '/Thunderstorm.png';
+import wind from '/wind.png';
+import fog from '/fog.png';
+import sunrise from '/sunrise.png';
+import sunset from '/sunset.png';
+import moonrise from '/moonrise.png';
+import moonset from '/moonset.png';
+import compass from '/compass.png';
+import gust from '/gust.png';
+import humidity from '/humidity.png';
+import feel from '/feel.png';
+import sunny_video from '/sunny.mp4';
+import rainy_video from '/rainy.mp4';
+import cloudy_video from '/cloudy.mp4';
 
 export {
     logo,
